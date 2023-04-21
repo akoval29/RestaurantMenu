289a77d9e03249faf4ea039989fa93d2b4c2a28f
Andriy Koval <akoval29@gmail.com>
2023-04-21 18:26:29 +0300

    no scale script

--- js/food.js
+++ js/food.js
@@ -1,87 +1,154 @@
 export const allFood = [
   {
-    name: "3 KG BIG MEAT COMPANY SET",
+    name: "Beef Burger",
+    type: "breakfast",
+    price: 15.5,
+    description:
+      "Non tempore cum, magni atque accusantium labore ab, laborum qui    error. Exercitationem voluptatem nulla quam iste et, molestiae maiores reiciendis vitae nam eum repellat quo consequatur nesciunt est facilis velit!",
+    imageSrc:
+      "https://d1e3z2jco40k3v.cloudfront.net/-/media/kamisua2018/recipes/800/hamburger_beef_barbecue_800.jpg?rev=9a1df6bdc80f4aa9a68355c852c8bfe6&vd=20200706T004057Z&hash=FACB3CBDFE36F53B8650A3AA18F67C31",
+  },
+  {
+    name: "Meat-egg breakfast set",
+    type: "breakfast",
+    price: 220.0,
+    description:
+      "Mollitia nesciunt doloremque harum vero, aspernatur eligendi, illum nemo cumque a repellat eius odit fuga! ",
+    imageSrc:
+      "https://reviewed-com-res.cloudinary.com/image/fetch/s---oGP6J6d--/b_white,c_fill,cs_srgb,f_auto,fl_progressive.strip_profile,g_auto,h_729,q_auto,w_972/https://reviewed-production.s3.amazonaws.com/1568123038734/Bfast.png",
+  },
+  {
+    name: "3 kg big meat company set",
+    type: "dinner",
     price: 2580.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Eos facilis reiciendis ipsum eveniet veniam, quod voluptatem totam mollitia? Quam, officiis reprehenderit, vitae suscipit mollitia molestias ex minus tempore, commodi voluptate sint soluta laboriosam doloremque eum nisi pariatur iure dicta ipsam.",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2022/12/photo_2022-12-13_15-05-08-300x300.jpg",
   },
   {
-    name: "DOUBLE FOCACCIA PULLED BEEF SET",
+    name: "Double focaccia pulled beef set",
+    type: "dinner",
     price: 490.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Dolor sit amet consectetur adipisicing elit. Exercitationem minima voluptate beatae doloremque, suscipit culpa illum officiis tempora.",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/img_1232-300x300.jpg",
   },
   {
-    name: "DOUBLE MEATBALLS HOTDOGS SET",
+    name: "Double meatballs hotdogs set",
+    type: "dinner",
     price: 680.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Mollitia nesciunt doloremque harum vero, aspernatur eligendi, illum nemo cumque a repellat eius odit fuga! ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/img_1266-300x300.jpg",
   },
   {
-    name: "DOUBLE ITALY BURGERS SET",
+    name: "Double italy burgers set",
+    type: "dinner",
     price: 685.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Sed maxime quisquam itaque nostrum, eum soluta maiores optio architecto unde illo facilis deleniti iusto ut. ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/img_1236-300x300.jpg",
   },
   {
-    name: "3 KG BIG ITALY SET",
+    name: "3 kg big italy set",
+    type: "dinner",
     price: 2100.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Culpa nobis laborum dolores incidunt similique ad maiores delectus vel. Fugit, asperiores? ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/img_1237-300x300.jpg",
   },
   {
-    name: "4 KG SUPER COMPANY SET",
+    name: "4 kg super company set",
+    type: "dinner",
     price: 2980.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Optio aspernatur nostrum atque, aliquam dolores, nulla quidem excepturi ad voluptas quia, perspiciatis natus. ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/03/photo_2023-03-02_19-13-29-300x300.jpg",
   },
   {
-    name: "DOUBLE STRIPS SET",
+    name: "Double strips set",
+    type: "lunch",
     price: 360.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Accusamus iure distinctio quo ea blanditiis commodi quas ab placeat asperiores, at natus. ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/01/img_0244-300x300.jpg",
   },
   {
-    name: "QUATRO BEEF CHEESE SET",
+    name: "Quatro beef cheese set",
+    type: "lunch",
     price: 640.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Labore corporis ad molestias asperiores repellendus explicabo suscipit est cupiditate. Asperiores necessitatibus vitae, magnam repudiandae explicabo id quibusdam doloribus ea non, similique laborum quis natus dolorum saepe corrupti sint.",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/img_1243-300x300.jpg",
   },
   {
-    name: "DOUBLE BRISKET & PASTRAMI",
+    name: "Double brisket & pastrami",
+    type: "lunch",
     price: 550.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Mollitia nesciunt doloremque harum vero, aspernatur eligendi, illum nemo cumque a repellat eius odit fuga! ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/img_1240-300x300.jpg",
   },
   {
-    name: "DOUBLE SEASIDE SET",
+    name: "Double seaside set",
+    type: "lunch",
     price: 777.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Incidunt aperiam numquam magnam qui saepe minima mollitia ipsum exercitationem! Doloremque nisi sint laborum, cupiditate blanditiis quia alias quibusdam optio atque laudantium, cum dolore sapiente consequuntur error eum voluptatem quaerat.",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2022/11/photo_2022-11-10_20-03-09-300x300.jpg",
   },
   {
-    name: "Трюфельна «сирна ванна»",
+    name: 'Truffle "cheese bath"',
+    type: "sauce",
     price: 120.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Culpa nobis laborum dolores incidunt similique ad maiores delectus vel. Fugit, asperiores? ",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2022/12/photo_2022-12-13_15-05-00-300x300.jpg",
   },
   {
-    name: "Томатно-м’ясна ванна",
+    name: "Tomato and meat bath",
+    type: "sauce",
     price: 79.0,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "Dolor sit amet consectetur adipisicing elit. Exercitationem minima voluptate beatae doloremque, suscipit culpa illum officiis tempora.",
     imageSrc:
       "https://smokers.kiev.ua/wp-content/uploads/2023/04/photo_2023-04-18_19-15-50-300x300.jpg",
+  },
+  {
+    name: "Shocolate cocktail",
+    type: "cocktail",
+    price: 23.0,
+    description:
+      "Mollitia nesciunt doloremque harum vero, aspernatur eligendi, illum nemo cumque a repellat eius odit fuga!",
+    imageSrc:
+      "https://i.obozrevatel.com/food/recipemain/2019/8/24/kyiup.jpg?size=636x424",
+  },
+  {
+    name: "Bloody Mary",
+    type: "cocktail",
+    price: 20.0,
+    description:
+      "Accusamus iure distinctio quo ea blanditiis commodi quas ab placeat asperiores, at natus.",
+    imageSrc: "https://dekatop.com/wp-content/uploads/2013/03/coctail_07.jpg",
+  },
+  {
+    name: "Mojito",
+    type: "cocktail",
+    price: 27.0,
+    description:
+      "Culpa nobis laborum dolores incidunt similique ad maiores delectus vel. Fugit, asperiores?",
+    imageSrc: "https://vsviti.com.ua/wp-content/uploads/2013/03/coctail_01.jpg",
   },
 ];
 // https://www.menu.anneson.com/
